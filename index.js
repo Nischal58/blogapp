21ba20cf73ce1a83256c4dfbc840e07785ef74ff
Nischal58 <neeschal100@gmail.com>
2022-05-23 21:55:57 +0545

    add changes

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 var tasklist= [
     {
         id:1,
-        blog:'First BLog ',
-        comment:'Nischal'
+        blog:{
+            title:'how to code',
+            content:'learn how to code'
+        },
+        comment:['Nice']
     },
     {
         id:2,
-        blog:'Second Blog ',
+        blog:{
+            title:'how to code',
+            content:'learn how to code'
+        },
+        comment:['Nice']
     },
     {
         id:3,
-        blog:'Third Blog',
+        blog:{
+            title:'how to code',
+            content:'learn how to code'
+        },
+        comment:['Nice']
     }
 ];
  ShowBlogs = ()=>{
     document.querySelector('#tasklist').innerHTML = '';
     tasklist.forEach((val,index)=>{
         document.querySelector('#tasklist').innerHTML += `
-        <h1>${val.blog}</h1>
-        <p>${val.comment}</p>
+        <h1>${val.blog.title}</h1>
+        <p>${val.blog.content}</p>
         <button onClick="getEditData(${index})">Edit</button>
         <button onclick="deleteTask(${val.id})">delete</button>
         `;
@@ -27,10 +38,14 @@ var tasklist= [
 ShowBlogs();
 const addBlogs=()=>{
     const id =tasklist[tasklist.length-1].id+1
-    const blog=document.querySelector('#addBlog').value;
+    // const blog=document.querySelector('#addBlog').value;
+    const title=document.querySelector('.add-title').value;
+    const content=document.querySelector('.add-content').value;
+
     tasklist.push({
-        blog,
+        id:id,blog:{title:title,content:content}
     });
+    console.log(tasklist);
 ShowBlogs();
 }
 const deleteTask = id=>{
